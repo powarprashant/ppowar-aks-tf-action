85ab9ade440d5f6a07af4d5eaeb2ca028cbb7745
Prashant Powar <prashant.powar@nitorinfotech.com>
2026-01-20 13:06:57 +0530

    added tf files

--- main.tf
+++ main.tf
@@ -13,3 +13,30 @@ terraform {
 provider "azurerm" {                                                                                                     
   features {}                                                                                                            
 }   
+# Create a Azure resource group
+resource "azurerm_resource_group" "aks-rg2" {                                                                          
+  name     = "aks-rg2-tf"                                                                                         
+  location = "Central US"                                                                                               
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks-demo"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "demo-aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "demoaks"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
