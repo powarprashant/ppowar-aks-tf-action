b4dff5e9f44710224258efbc103907189e186c80
Prashant Powar <prashant.powar@nitorinfotech.com>
2026-01-22 13:38:21 +0530

    added resource group name

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,6 @@
-
-#output "resource_group_name" {
- # value = azurerm_resource_group.rg.name
-#}
+output "resource_group_name" {
+ value = azurerm_resource_group.rg.name
+}
 
 output "aks_name" {
   value = azurerm_kubernetes_cluster.aks.name
