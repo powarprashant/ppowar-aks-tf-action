95a68ea436613f559dc366cc7693f4614065bb77
powarprashant <powarprashant>
2026-01-28 09:40:14 +0000

    modified provider.tf file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.117.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   use_oidc = true
